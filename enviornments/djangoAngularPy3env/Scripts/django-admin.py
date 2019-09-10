47a24a3220fa80750f30ba25de97970a221dcafd
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-09-09 20:47:01 -0600

    some minor/MAJOR refactoring.

--- enviornments/djangoAngularPy3env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\mrich\documents\prgramming\coding_dojo\personal_projects\portfoliowebsite\enviornments\djangoangularpy3env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
