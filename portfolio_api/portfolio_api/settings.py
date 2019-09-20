41ee9500bbc3cdeaa5ebec9fdf03f0317f409729
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-09-19 19:25:03 -0600

    add email service to angular.
    also hooked up djongo (mongo) to the api. Next I will need to inject that servie into the resum component and the contact component. also need to write the crud methods that will be in charge of creating the database entries.

--- portfolio_api/portfolio_api/settings.py
+++ portfolio_api/portfolio_api/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'portfolio_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'portfolio'
     }
 }
 
