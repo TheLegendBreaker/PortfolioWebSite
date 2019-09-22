62ed77c40773e38fe9b5d727c8a263ef23b1a129
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-09-21 19:41:43 -0600

    Dialog pops up and closes as expected.
    Need to figure out positioning. Also setting the Dialog Config.disableClose = false is not working as I expected.
    The dialog does not close when I click off the dialog box. also if I hit the button to open it I can open more than one dialog. I got some kinks to work out

--- portfolio_api/meanMadePortfolio/src/app/sendResume/send-resume-button/send-resume-button.component.ts
+++ portfolio_api/meanMadePortfolio/src/app/sendResume/send-resume-button/send-resume-button.component.ts
@@ -1,15 +1,25 @@
+import { MatDialog, MatDialogConfig } from '@angular/material';
 import { Component, OnInit } from '@angular/core';
+
+import { SendResumeDialogComponent } from '../send-resume-dialog/send-resume-dialog.component';
 
 @Component({
   selector: 'app-send-resume-button',
   templateUrl: './send-resume-button.component.html',
-  styleUrls: ['./send-resume-button.component.css']
+  styleUrls: ['./send-resume-button.component.css'],
 })
 export class SendResumeButtonComponent implements OnInit {
+  constructor(private dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  OpenDialog() {
+    const config = new MatDialogConfig();
+
+    config.disableClose = false;
+    config.autoFocus = true;
+    config.hasBackdrop = false;
+
+    this.dialog.open(SendResumeDialogComponent, config);
   }
-
 }
--- portfolio_api/meanMadePortfolio/src/app/sendResume/send-resume-dialog/send-resume-dialog.component.ts
+++ portfolio_api/meanMadePortfolio/src/app/sendResume/send-resume-dialog/send-resume-dialog.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
+import { Email } from 'src/app/interface';
 
 @Component({
   selector: 'app-send-resume-dialog',
   templateUrl: './send-resume-dialog.component.html',
-  styleUrls: ['./send-resume-dialog.component.css']
+  styleUrls: ['./send-resume-dialog.component.css'],
 })
 export class SendResumeDialogComponent implements OnInit {
+  email = new Email();
+  constructor(private dialogRef: MatDialogRef<SendResumeDialogComponent>) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  close() {
+    this.dialogRef.close();
   }
-
 }
