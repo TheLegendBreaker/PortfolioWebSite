d27afb355ac231a786d8b9a6c8c0e3864b002330
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-09-24 15:05:01 -0600

    Auto scroll on show page working.
    now on to getting the call to action component up and running. I think I can do it with a boolean property and and if in the component's html tag.

--- portfolio_api/meanMadePortfolio/src/app/showPage/show-project/show-project.component.ts
+++ portfolio_api/meanMadePortfolio/src/app/showPage/show-project/show-project.component.ts
@@ -9,16 +9,37 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ShowProjectComponent implements OnInit, OnDestroy {
   display: any[];
+  autoNav: any;
+  navPress = false;
+  lastDirection = 'Right';
   constructor(
     private readonly projServ: ProjectService,
     private readonly route: ActivatedRoute
   ) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.autoScroll();
+  }
   scroll(direction: string): void {
     this.projServ.scroll(direction);
+    this.clientNavPress();
+    this.lastDirection = direction;
     console.log(`direction`, direction);
+  }
+  private autoScroll() {
+    this.autoNav = setInterval(() => {
+      if (!this.navPress) {
+        this.projServ.scroll(this.lastDirection);
+      }
+    }, 3500);
+  }
+  private clientNavPress() {
+    this.navPress = true;
+    setInterval(() => {
+      this.navPress = false;
+    }, 8000);
   }
   ngOnDestroy(): void {
     this.projServ.direction = null;
+    clearInterval(this.autoNav);
   }
 }
