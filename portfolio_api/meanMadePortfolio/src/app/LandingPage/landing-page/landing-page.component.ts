963ea7407c0e6da5d77a76a3917ae49819f3440d
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-09-24 10:37:56 -0600

    Auto scroll working as expected.
    Now to add the focus UI for the landing page reel. Hopefully the change of screen size wont screw with neighboring elements too much. Then I need to add the call to action component for show page. And I also need to test out how to modularize the animation transitions.

--- portfolio_api/meanMadePortfolio/src/app/LandingPage/landing-page/landing-page.component.ts
+++ portfolio_api/meanMadePortfolio/src/app/LandingPage/landing-page/landing-page.component.ts
@@ -1,16 +1,18 @@
 import { ProjectsService } from 'src/app/services/projects.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css'],
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, OnDestroy {
   // set up a property to display the project blurb
   blurb: string;
-
+  lastDirection = 'Up';
   links: any[] = [];
+  navPress = false;
+  autoNav: any;
   constructor(private readonly projServ: ProjectsService) {}
 
   ngOnInit() {
@@ -18,11 +20,38 @@ export class LandingPageComponent implements OnInit {
     this.projServ.useLinks$.subscribe(links => {
       this.links = links;
     });
+    this.autoScroll();
   }
 
   scroll(direction: string) {
     console.log('scroll on landing page');
+    this.clientNavPress();
+    this.lastDirection = direction;
     this.projServ.scroll(direction);
   }
   // build out an autoScroll for the change of reel state.
+  private autoScroll() {
+    this.autoNav = setInterval(() => {
+      if (!this.navPress) {
+        this.projServ.scroll(this.lastDirection);
+      }
+    }, 5000);
+  }
+  private whichDirection() {
+    if (this.projServ.direction === undefined) {
+      this.projServ.scroll('Up');
+    } else {
+      this.projServ.scroll(this.lastDirection);
+    }
+  }
+
+  private clientNavPress() {
+    this.navPress = true;
+    setInterval(() => {
+      this.navPress = false;
+    }, 10000);
+  }
+  ngOnDestroy() {
+    clearInterval(this.autoNav);
+  }
 }
