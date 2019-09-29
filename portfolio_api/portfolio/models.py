f1abf6d0d3c15fda3988cf211900768f7f7b2b19
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-09-28 21:22:37 -0600

    Got my first djongo model working.
     how django's orm returns data. I think if I made the relationship between projects match the dll Iin my dll. Oh, I am commited to mongo now. Next is to set up routes to handle the crud opertaions for my data.

--- portfolio_api/portfolio/models.py
+++ portfolio_api/portfolio/models.py
@@ -1,2 +1,21 @@
-from django.db import models
+from djongo import models
+
+
+class StringArrayManager(models.DjongoManager):
+    def create_instance(self):
+        return self.create(
+            strings = []
+        )
+
+class Project(models.Model):
+    # id
+    title = models.CharField(max_length=45)
+    blurbs = models.ListField()
+    images = models.ListField()
+    links = models.ListField()
+     
+    
+    objects = models.DjongoManager()
+# Rather than create another model for all projects, project controller will format all projects into a single json for the frontend.
+
 
