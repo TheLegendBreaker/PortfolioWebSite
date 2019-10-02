41630243ccdbbdf69602489701e090dca27de5ed
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-10-01 20:46:17 -0600

    All buttons on the site are working as expected.
    I need to give a disable state for my send buttons. also need to disable the send button on the contact page so that you can't activate if any of the form's fields are empty. Next big thing is a resume refactor.

--- portfolio_api/meanMadePortfolio/src/app/nav/nav/nav.component.ts
+++ portfolio_api/meanMadePortfolio/src/app/nav/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { RouteAnimationsService } from 'src/app/services/route-animations.service';
+import { ProjectApiService } from 'src/app/services/project-api.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,12 +11,20 @@ import { RouteAnimationsService } from 'src/app/services/route-animations.servic
   encapsulation: ViewEncapsulation.None,
 })
 export class NavComponent implements OnInit {
+  projIds: number[] = [];
   constructor(
-    private readonly routeAnimationServ: RouteAnimationsService,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly projApiServ: ProjectApiService,
+    private readonly routeAnimationServ: RouteAnimationsService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.projApiServ.getProjects().subscribe(projects => {
+      for (const project of projects.projects) {
+        this.projIds.push(project.id);
+      }
+    });
+  }
   private navToHome(): void {
     this.routeAnimationServ.changeState();
     this.router.navigateByUrl('/portfolio');
