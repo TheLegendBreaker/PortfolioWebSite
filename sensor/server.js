02e66f3e7b41836d46a11be360bc0549e604d5dd
TheLegendBreaker <thegreatcarradio@yahoo.com>
2019-11-17 16:34:38 -0700

    Sensor server is now redirect to a different port based on device
    So looking for a user-agent node package I found express-useragent. It seems to handle device detection completely. It seems now that I dont need 51Degrees. Now it is on to making a moblie version of the site. This might be a good chance to learn react rather start up another angular project for it.

--- sensor/server.js
+++ sensor/server.js
@@ -1,22 +1,44 @@
 const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
-const 51d = require('fiftyonedegreescore');
-
+const D = require('fiftyonedegreescore');
+const userAgent = require('express-useragent');
 const route = require('express').Router();
 
-const 51config = {
-  'dataFile' : require('fiftyonedegreeslitetrie'),
+const app = express();
+
+const{
+  env: {PORT:port = 5000},
+} = process;
+
+const config = {
+  'dataFile' : 'data/51Degrees-LiteV3.4.trie', 
   'properties' : 'IsMobile',
   'stronglyTyped' : true
 }
 
-const 51provider = new 51d.provider(51config);
+const provider = new D.provider(config);
 
 
 route.all('*', function(request,response){ 
+  const source = request.headers['user-agent'];
+  const agent = userAgent.parse(source);
+  
+  const match = provider.getMatch(agent);
+  console.log('here is the agent', agent); 
+  if(agent .isModile){
+    console.log('mobile');
+    response.redirect('http://localhost:5002/');
+  }else{
+    console.log('not mobile');
+    response.redirect('http://localhost:5001/');
+  }
   // 51d checks device
   // redirects to proper app
-  pass
 });
+
+app.use(route);
+
+app.listen( port, ()=>{console.log(`listening on port ${port}`)});
+
 
